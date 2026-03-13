c4977a1fdd25af823ced01b0af22af1b462c9c20
string <348040933@qq.com>
2026-03-13 16:54:52 +0800

    render-engine: add default media and embed renderers

--- packages/view-canvas/src/defaultRenderers/index.ts
+++ packages/view-canvas/src/defaultRenderers/index.ts
@@ -1,22 +1,33 @@
 import {
+  audioRenderer,
   blockquoteRenderer,
+  bookmarkRenderer,
   bulletListRenderer,
   codeBlockRenderer,
+  fileRenderer,
   headingRenderer,
   horizontalRuleRenderer,
   orderedListRenderer,
   paragraphRenderer,
   taskListRenderer,
+  webPageRenderer,
 } from "lumenpage-render-engine";
+import { createDefaultAudioNodeView } from "./audio";
+import { createDefaultBookmarkNodeView } from "./bookmark";
+import { createDefaultFileNodeView } from "./file";
 import { createDefaultImageNodeView, imageRenderer } from "./image";
 import { createTableSelectionGeometry, tableRenderer } from "./table";
 import { createDefaultVideoNodeView, videoRenderer } from "./video";
+import { createDefaultWebPageNodeView } from "./webPage";
 
 const defaultNodeRenderers: Record<string, any> = {
+  audio: audioRenderer,
+  bookmark: bookmarkRenderer,
   paragraph: paragraphRenderer,
   heading: headingRenderer,
   blockquote: blockquoteRenderer,
   codeBlock: codeBlockRenderer,
+  file: fileRenderer,
   horizontalRule: horizontalRuleRenderer,
   bulletList: bulletListRenderer,
   orderedList: orderedListRenderer,
@@ -24,6 +35,7 @@ const defaultNodeRenderers: Record<string, any> = {
   image: imageRenderer,
   video: videoRenderer,
   table: tableRenderer,
+  webPage: webPageRenderer,
 };
 
 export const createDefaultTableSelectionGeometry = createTableSelectionGeometry;
@@ -37,6 +49,10 @@ export const getDefaultNodeRenderer = (nodeName?: string | null) => {
 };
 
 export {
+  createDefaultAudioNodeView,
+  createDefaultBookmarkNodeView,
+  createDefaultFileNodeView,
   createDefaultImageNodeView,
   createDefaultVideoNodeView,
+  createDefaultWebPageNodeView,
 };
--- packages/view-canvas/src/index.ts
+++ packages/view-canvas/src/index.ts
@@ -3,7 +3,11 @@ export * from "./extensionRuntime";
 export * from "./view/index";
 export * as layoutPagination from "./layout-pagination/index";
 export {
+  createDefaultAudioNodeView,
+  createDefaultBookmarkNodeView,
+  createDefaultFileNodeView,
   createDefaultImageNodeView,
   createDefaultTableSelectionGeometry,
   createDefaultVideoNodeView,
+  createDefaultWebPageNodeView,
 } from "./defaultRenderers/index";
